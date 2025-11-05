926776ef160d699e6c7c30298ee6bc807c8c170b
dieeinetanne <saskia.schnorf@gmail.com>
2025-11-05 17:17:27 +0100

    ANIMATION

--- Aufgabe-2-V1/sketch.js
+++ Aufgabe-2-V1/sketch.js
@@ -2,12 +2,14 @@
 let valueSlider;
 
 let drehwinkel = 0;
-let flipSpeed = 5;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  valueSlider = createSlider(-10, 38, 9);
-  valueSlider.position(width - valueSlider.width - 20, height - valueSlider.height - 20);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+valueSlider = createSlider(0, 100, 20);
+valueSlider.position(width - valueSlider.width - 400, height - valueSlider.height - 20);
+//den Slider verlängern
+valueSlider.size(500);
+
   angleMode(DEGREES);
   rectMode(CORNER);
   noStroke();
@@ -15,7 +17,7 @@ function setup() {
 
 function draw() {
 //Slider
-  let inputValue = valueSlider.value();
+let inputValue = valueSlider.value();
 
   let inputMin = -10;
   let inputMax = 38;
@@ -23,20 +25,29 @@ function draw() {
   let outputMin = 0;
   let outputMax = 255;
 
-  background(0, 0, 0, 10);
+  background(0, 0, 0, 255);
 
 
-  let alpha = map(inputValue, inputMin, inputMax, outputMin, outputMax, 0, 255);
+ let alpha = map(inputValue, inputMin, inputMax, outputMin, outputMax, 0, 255);
 
 //Rotation und Zeichnen der Karte
   push();
   rotateY(drehwinkel);
+  
+//fill mit regenbogenfarben abhängig vom Sliderwert
+  let r = map(inputValue, inputMin, inputMax, 255, 0);
+  let g = map(inputValue, inputMin, inputMax, 0, 255);
+  let b = map(inputValue, inputMin, inputMax, 0, 255);
+  fill(r, g, b);
+  
+ // fill(191, 239, 255);
 
+  //Zeichnet ein Rechtech welches mittig platziert ist
+  rectMode(CENTER);
   rect(0, 0, 200, 400);
-  fill(191, 239, 255, alpha);
   
   pop();
 
-
-  drehwinkel = drehwinkel + 1;
+//Drehung abhängig vom Sliderwert
+  drehwinkel = drehwinkel + alpha * 0.1;
 }
